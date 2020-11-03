a6efb18e25e3ea2a0deaae79ea9baf2200cd2024
fortanna12 <59663200+fortanna12@users.noreply.github.com>
2020-11-03 18:26:45 -0500

    MidterTuesday

---
+++ JackFarrellMidterm2/src/midtermTuesday/Engineer.java
@@ -0,0 +1,38 @@
+package midtermTuesday;
+
+
+/**
+ *
+ * @author haki
+ */
+public class Engineer {
+    
+    private int yearsOfService;
+    private String name;
+    private double salary;
+
+    public int getYearsOfService() {
+        return yearsOfService;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setYearsOfService(int yearsOfService) {
+        this.yearsOfService = yearsOfService;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+}
